962b674d4b367b215b780e1ac6f77aea42a145ad
Hongwei Hu <huonwe@huonwe-OMEN.localdomain>
2022-05-21 23:13:01 +0800

    添加退出登录

--- HTMLStatic/js/utils.js
+++ HTMLStatic/js/utils.js
@@ -11,3 +11,12 @@ function commonXHRHandler (xhr, ev){
         return;
     }
 }
+
+function clearAllCookie() {
+    var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+    if(keys) {
+        for(var i = keys.length; i--;)
+            document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
+    }
+    location.reload()
+}
